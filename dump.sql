fd294ec0425938f5575f43f20fa7c63568b4c3ee
Repairman Jack <lxtr@rambler.ru>
2018-07-06 19:27:10 +0300

    sql

--- dump.sql
+++ dump.sql
@@ -1,3 +1,19 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.1.73-community : Database - test
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `transactions` */
 
 DROP TABLE IF EXISTS `transactions`;
 
@@ -11,9 +27,11 @@ CREATE TABLE `transactions` (
   CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+/*Data for the table `transactions` */
 
 insert  into `transactions`(`id`,`user_id`,`summ`,`datetime`) values (1,1,'100500.00','2018-07-06 15:45:42');
 
+/*Table structure for table `users` */
 
 DROP TABLE IF EXISTS `users`;
 
@@ -25,5 +43,11 @@ CREATE TABLE `users` (
   UNIQUE KEY `login` (`login`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+/*Data for the table `users` */
 
 insert  into `users`(`id`,`login`,`passw`) values (1,'admin','21232f297a57a5a743894a0e4a801fc3');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
